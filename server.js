b6aae223bb146ea822abae96c06a8f381f23705b
joshuad1590 <joshuad15@gmail.com>
2022-04-17 23:26:23 -0400

    Add Heroku

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+const {animals} = require('./data/animals.json');
+
+function filterByQuery(query, animalsArray) {
+    let personalityTraitsArray = [];
+    let filteredResults = animalsArray;
+    if (query.personalityTraitsArray) {
+        if (typeof query.personalityTraitsArray === 'string') {
+            personalityTraitsArray = [query.personalityTraitsArray];
+        } else {
+            personalityTraitsArray = query.personalityTraitsArray;
+        }
+        personalityTraitsArray.forEach(trait => {
+            filteredResults = filteredResults.filter(
+                animal => animal.personalityTraitsArray.indexOf(trait) !== -1
+            );
+        });
+    }
+    if (query.diet) {
+        filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
+    }
+    if (query.species) {
+        filteredResults = filteredResults.filter(animal => animal.species === query.species);
+    }
+    if (query.name) {
+        filteredResults = filteredResults.filter(animal => animal.name === query.name);
+    }
+    return filteredResults;
+}
+app.get('/api/animals', (req, res) => {
+    let results = animals;
+    if (req.query) {
+        results = filterByQuery(req.query, results);
+    }
+    res.json(results);
+});
+
+app.listen(3001, () => {
+    console.log('API server now on port 3001!');
+});
