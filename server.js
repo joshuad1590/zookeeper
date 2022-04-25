985b38eb552a5df615298994ec9112d200f8959d
joshuad1590 <joshuad15@gmail.com>
2022-04-24 23:18:33 -0400

    front-end

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const {animals} = require('./data/animals.json');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json()); 
+app.use(express.static('public'));
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
@@ -101,3 +102,18 @@ app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 });
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+  });
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
